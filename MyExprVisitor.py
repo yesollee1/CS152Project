516845a1bcfe941244d239b59052064cfb372bb7
VolTorian <voltorian.social@gmail.com>
2023-05-07 12:23:18 -0700

    divide by zero
    dividing by zero seems to be handled

--- MyExprVisitor.py
+++ MyExprVisitor.py
@@ -5,6 +5,7 @@ class MyExprVisitor(ExprVisitor):
     def __init__(self):
         super(MyExprVisitor, self).__init__()
         self.stack = []  # Stack to evaluate the expression
+        self.divided_by_zero = False
 
     # Visit a parse tree produced by ExprParser#prog.
     def visitProg(self, ctx:ExprParser.ProgContext):
@@ -12,9 +13,14 @@ class MyExprVisitor(ExprVisitor):
 
     # Visit a parse tree produced by ExprParser#infixExpr.
     def visitInfixExpr(self, ctx:ExprParser.InfixExprContext):
+        # if self.divided_by_zero:
+        #     return "Can't divide by zero!"
         self.visit(ctx.left)  # Evaluate the left  expression and push to stack
         self.visit(ctx.right) # Evaluate the right expression and push to stack
 
+        check = self.stack[-1]
+        if check == "Can't divide by zero!":
+            return "Can't divide by zero!"
         b = self.stack.pop()  # Why is ‘b’ the first popped item?
         a = self.stack.pop()
         c = None
@@ -24,7 +30,11 @@ class MyExprVisitor(ExprVisitor):
         elif ctx.OP_MUL():
             c = a * b
         elif ctx.OP_DIV():
-            c = a / b
+            try:
+                c = a / b
+            except ZeroDivisionError:
+                c = "Can't divide by zero!"
+                self.divided_by_zero = True
         elif ctx.OP_ADD():
             c = a + b
         elif ctx.OP_SUB():
@@ -36,6 +46,8 @@ class MyExprVisitor(ExprVisitor):
 
     # Visit a parse tree produced by ExprParser#numberExpr.
     def visitNumberExpr(self, ctx:ExprParser.NumberExprContext):
+        if self.divided_by_zero:
+            return
         c = int(str(ctx.INT()))  # Found a number, just insert to stack
         self.stack.append(c)
         return c
